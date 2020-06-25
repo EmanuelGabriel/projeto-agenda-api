9c9ab7dbac6e16bf451b97ff09c70b94d0c82446
Samuel Gabriel <emanuel.gabriel.sousa@protonmail.com>
2020-06-24 23:23:51 -0300

    alterando arquivos de versionamento da base de dados com flyway

--- src/main/resources/db/migration/V006__adicionar_dados_tabela_cliente.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO cliente (nome, email, cpf, ativo) VALUES('Fernando Henrique Souza', 'fernandohenrique@gmail.com.br', '027827632733', false);
-INSERT INTO cliente (nome, email, cpf, ativo) VALUES ('Solano Alvim','solano_alvim@terra.com.br', '93969823755', false);
-INSERT INTO cliente (nome, email, cpf, ativo) VALUES ('Augusto Ramires','augusto_ramires@yahoo.com', '988652378733', true);
---
+++ src/main/resources/db/migration/V010__adicionar-dados-tabela-cliente.sql
@@ -0,0 +1,3 @@
+INSERT INTO cliente (nome, email, cpf, ativo) VALUES('Fernando Henrique Moreira', 'fernandohenrique@gmail.com.br', '90873643003', false);
+INSERT INTO cliente (nome, email, cpf, ativo) VALUES ('Solano Alvim Abdubo','solano-alvim@terra.com.br', '79805121038', false);
+INSERT INTO cliente (nome, email, cpf, ativo) VALUES ('Augusto Ramires Cavalcante','augusto-ramires@yahoo.com', '79069505002', true);
