c085f1dc86d2e7eb9cca3e88e484f6a50271f124
Samuel Gabriel <emanuel.gabriel.sousa@protonmail.com>
2020-07-16 21:28:46 -0300

    criação da entidade de usuário

--- src/main/java/br/com/srsolution/agenda/domain/model/Usuario.java
+++ src/main/java/br/com/srsolution/agenda/domain/model/Usuario.java
@@ -1,9 +1,16 @@
 package br.com.srsolution.agenda.domain.model;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -14,10 +21,19 @@ public class Usuario implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
+
+	@NotBlank
+	@Size(min = 5, max = 90)
 	private String nome;
+
+	@Email
 	private String email;
+
 	private String senha;
 	private Boolean ativo;
 
+	@Column(name = "data_cadastro")
+	private LocalDateTime dataCadastro;
 }
