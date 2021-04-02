20b0c992dad64d8ad9be38faaa995da1e9375e51
Samuel Gabriel Sousa <emanuel.gabriel.sousa@protonmail.com>
2021-04-02 14:39:34 -0300

    refatorando as propriedades da documentacao com swagger

--- src/main/java/br/com/srsolution/agenda/api/dtos/request/ClienteModelInputRequest.java
+++ src/main/java/br/com/srsolution/agenda/api/dtos/request/ClienteModelInputRequest.java
@@ -8,6 +8,7 @@ import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.br.CPF;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,14 +18,17 @@ public class ClienteModelInputRequest implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Schema(name = "nome", type = "string", format = "string", description = "nome do cliente", required = true, example = "Fulano")
 	@NotBlank
 	@Size(min = 5, max = 90)
 	private String nome;
 
+	@Schema(name = "email", type = "string", format = "string", description = "email do cliente", required = true, example = "email@email.com.br")
 	@Email
 	@NotBlank
 	private String email;
 
+	@Schema(name = "cpf", type = "string", format = "string", description = "CPF do cliente", required = true, example = "000.000.000-00")
 	@CPF
 	@NotBlank
 	private String cpf;
--- src/main/java/br/com/srsolution/agenda/api/dtos/request/ClienteModelParcialRequest.java
+++ src/main/java/br/com/srsolution/agenda/api/dtos/request/ClienteModelParcialRequest.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,9 +15,11 @@ public class ClienteModelParcialRequest implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Schema(name = "nome", type = "string", format = "string", description = "nome do cliente", required = true, example = "Fulano")
 	@NotBlank
 	private String nome;
 
+	@Schema(name = "email", type = "string", format = "string", description = "e-mail do cliente", required = true, example = "email@email.com.br")
 	@Email
 	private String email;
 
